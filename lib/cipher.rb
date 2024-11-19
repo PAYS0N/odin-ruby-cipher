784bf0702e9aa21fe6cbff4048e11faf3a63980d
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-18 21:12:05 -0600

    Refactor main files into main and class
    Move class out of main file to allow for separate
    testing. Create first test.

---
+++ lib/cipher.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module OdinRubyCipher
+  # class to hold the cipher function
+  class Cipher
+    def caesar_cipher(string, num)
+      shifted_string_arr = string.chars.map do |char|
+        if char.between?("a", "z")
+          ordinal = char.ord + num
+          ordinal -= 26 while ordinal > "z".ord
+          ordinal.chr
+        elsif char.between?("A", "Z")
+          ordinal = char.ord + num
+          ordinal -= 26 while ordinal > "Z".ord
+          ordinal.chr
+        else
+          char
+        end
+      end
+      shifted_string_arr.join
+    end
+  end
+end
--- spec/odin_ruby_cipher_spec.rb
+++ spec/odin_ruby_cipher_spec.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-require_relative('../lib/odin_ruby_cipher')
+require_relative("../lib/cipher")
 
 describe OdinRubyCipher::Cipher do
+  context "when using the cipher" do
+    subject(:cipher) { described_class.new }
+
+    it "returns empty string when not given string" do
+      string = ""
+      expect(cipher.caesar_cipher(string, 5)).to eq("")
+    end
+  end
 end
