a178bb29e79fbc0550d601ce95bd3e00b9c724c6
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-18 21:07:03 -0600

    Refactor cipher function into module

--- lib/odin_ruby_cipher.rb
+++ lib/odin_ruby_cipher.rb
@@ -1,18 +1,26 @@
-def caesar_cipher(string, num)
-  shifted_string_arr = string.split('').map do |char|
-    if char.between?('a', 'z')
-      ordinal = char.ord + num
-      ordinal -= 26 while ordinal > 'z'.ord
-      ordinal.chr
-    elsif char.between?('A', 'Z')
-      ordinal = char.ord + num
-      ordinal -= 26 while ordinal > 'Z'.ord
-      ordinal.chr
-    else
-      char
+# frozen_string_literal: true
+
+module OdinRubyCipher
+  # class to hold the cipher function
+  class Cipher
+    def caesar_cipher(string, num)
+      shifted_string_arr = string.chars.map do |char|
+        if char.between?('a', 'z')
+          ordinal = char.ord + num
+          ordinal -= 26 while ordinal > 'z'.ord
+          ordinal.chr
+        elsif char.between?('A', 'Z')
+          ordinal = char.ord + num
+          ordinal -= 26 while ordinal > 'Z'.ord
+          ordinal.chr
+        else
+          char
+        end
+      end
+      shifted_string_arr.join
     end
   end
-  shifted_string_arr.join('')
 end
 
-puts caesar_cipher('Hello to The Odin Project!', 17)
+cipher = OdinRubyCipher::Cipher.new
+puts cipher.caesar_cipher('Hello to The Odin Project!', 17)
--- spec/odin_ruby_cipher_spec.rb
+++ spec/odin_ruby_cipher_spec.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
 require_relative('../lib/odin_ruby_cipher')
+
+describe OdinRubyCipher::Cipher do
+end
