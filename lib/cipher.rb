e92aa35f739c77bfd29fe82bcf7366239c8b139d
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-18 21:26:03 -0600

    Refactor and update function to pass new test

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -7,17 +7,25 @@ module OdinRubyCipher
       shifted_string_arr = string.chars.map do |char|
         if char.between?("a", "z")
           ordinal = char.ord + num
-          ordinal -= 26 while ordinal > "z".ord
+          ordinal = center_value(ordinal, "a".ord, "z".ord)
           ordinal.chr
         elsif char.between?("A", "Z")
           ordinal = char.ord + num
-          ordinal -= 26 while ordinal > "Z".ord
+          ordinal = center_value(ordinal, "A".ord, "Z".ord)
           ordinal.chr
         else
           char
         end
       end
       shifted_string_arr.join
+    end
+
+    def center_value(num, min, max)
+      until num.between?(min, max)
+        num -= 26 if num > max
+        num += 26 if num < min
+      end
+      num
     end
   end
 end
