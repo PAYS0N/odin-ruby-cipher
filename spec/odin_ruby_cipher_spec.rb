bccb9ec7765f881be24a7a548f58d1568fa9ee9d
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-18 21:15:16 -0600

    Create test for normal operation

--- spec/odin_ruby_cipher_spec.rb
+++ spec/odin_ruby_cipher_spec.rb
@@ -10,5 +10,12 @@ describe OdinRubyCipher::Cipher do
       string = ""
       expect(cipher.caesar_cipher(string, 5)).to eq("")
     end
+
+    it "returns correct string when gives normal values" do
+      string = "Hello to The Odin Project!"
+      shift = 17
+      answer = "Yvccf kf Kyv Fuze Gifavtk!"
+      expect(cipher.caesar_cipher(string, shift)).to eq(answer)
+    end
   end
 end
